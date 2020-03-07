6ad3fe368ee5c5ae5dabb32692957e2f677b51e3
EZvyagintsev <zvyagintsev.e@gmail.com>
2020-03-07 23:15:44 +0300

    log4j.properties added

--- TestMaven/src/main/java/TestMaven.java
+++ TestMaven/src/main/java/TestMaven.java
@@ -1,2 +1,18 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TestMaven {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestMaven.class);
+
+
+    public static void main( String[] args )
+    {
+        System.out.println( "My Maven test project" );
+        LOG.info("Hello this is test!");
+        LOG.error("This is error message!");
+        LOG.trace("This is trace message!");
+        LOG.warn("This is warn message!");
+
+    }
 }
